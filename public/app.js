7d108c01574643b282f9a68ae7815ec1c29b935e
Egor Kukanov <balovenisydibbl@gmail.com>
2023-10-17 20:54:19 +0300

    feat: added images, creating car list

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ const textGarage = document.createElement("p");
 const textPage = document.createElement("p");
 const carList = document.createElement("div");
 
+
 buttonGarage.type = "submit";
 buttonGarage.textContent = "TO GARAGE";
 buttonGarage.className = "garage-button";
@@ -68,7 +69,7 @@ textGarage.textContent = 'GARAGE (0)';
 textPage.id = 'pageText';
 textPage.textContent = 'Page #1'
 
-carList.id = "car-list";
+carList.id = "carList";
 
 pagesForm.appendChild(buttonGarage);
 pagesForm.appendChild(buttonWinners);
@@ -91,14 +92,68 @@ document.body.appendChild(pagesForm);
 document.body.appendChild(createForm);
 document.body.appendChild(updateForm);
 document.body.appendChild(controllersForm);
-document.body.appendChild(carList);
 document.body.appendChild(textGarage);
 document.body.appendChild(textPage);
+document.body.appendChild(carList);
 
 // controllers
 
 let count = 0;
+
 buttonCreate.addEventListener('click', () => {
+    const row1 = document.createElement("div");
+    const row2 = document.createElement("div");
+
+    const selectButton = document.createElement("button");
+    selectButton.type = "button"
+    selectButton.className = "select";
+    selectButton.textContent = "Select";
+
+    const removeButton = document.createElement("button");
+    removeButton.type = "button";
+    removeButton.className = "remove";
+    removeButton.textContent = "Remove";
+
+    const carName = document.createElement("span");
+    carName.className = "car-name";
+    carName.textContent = inputNameCreate.value;
+
+    row1.appendChild(selectButton);
+    row1.appendChild(removeButton);
+    row1.appendChild(carName);
+
+    const buttonA = document.createElement("button");
+    buttonA.type = "button"
+    buttonA.className = "A";
+    buttonA.textContent = "A";
+
+    const buttonB = document.createElement("button");
+    buttonB.type = "button"
+    buttonB.className = "B";
+    buttonB.textContent = "B";
+
+    const carImage = document.createElement("img");
+    carImage.className = "car-image"
+    carImage.src = "./assets/car.svg";
+    carImage.alt = "car-image";
+
+    const flagImage = document.createElement("img");
+    flagImage.className = "flag-image"
+    flagImage.src = "./assets/flag.png";
+    flagImage.alt = "flag-image";
+
+    row2.appendChild(buttonA);
+    row2.appendChild(buttonB);
+    row2.appendChild(carImage);
+    row2.appendChild(flagImage);
+
+    const lineDashed = document.createElement("div");
+    lineDashed.className = 'line-dashed'
+
+    carList.appendChild(row1);
+    carList.appendChild(row2);
+    carList.appendChild(lineDashed);
+
     count += 1;
     garageText.textContent = `GARAGE (${count})`;
     if (count > 7) {
